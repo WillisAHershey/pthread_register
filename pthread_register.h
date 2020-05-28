4e28052fc15d2300a277ab66bc42fb9df257ae80
Willis A. Hershey <48898590+WillisAHershey@users.noreply.github.com>
2020-05-27 20:09:44 -0400

    Create pthread_register.h

---
+++ pthread_register.h
@@ -0,0 +1,19 @@
+#ifndef PTHREAD_REGISTER
+#define PTHREAD_REGISTER
+
+#include <stdlib.h>
+#include <pthread.h>
+
+#define REGISTER 0
+#define RECALL 1
+#define DEREGISTER 2
+
+typedef struct reglistStruct{
+  struct reglistStruct *next;
+  void *reg;
+  pthread_t tid;
+}reglist;
+
+void* pthread_register(int,void*);
+
+#endif
